65fc9b5ebafcb60d6276dbbb66b8f1465d91be49
Filip Milojevic <filipmilojevic@Filips-MacBook-Air.local>
2025-06-23 23:50:37 +1000

    Fix studio page client boundary

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -6,13 +6,10 @@
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
+'use client'
 
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
-
-export const dynamic = 'force-static'
-
-export { metadata, viewport } from 'next-sanity/studio'
+import config from '@/sanity.config'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
