fe36d2ea5b0f9b5711f9991fac78b6c5710b24e2
Filip Milojevic <filipmilojevic@Filips-MacBook-Air.local>
2025-06-24 07:53:22 +1000

    Describe edit

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import Hero from '@/components/Hero'
 import Features from '@/components/Features'
 import Testimonials from '@/components/Testimonials'
 import DownloadCTA from '@/components/DownloadCTA'
+import StructuredData from '@/components/StructuredData'
+
 
 export default function Home() {
   return (
     <>
+     <StructuredData />
       <Hero />
       <Features />
       <Testimonials />
